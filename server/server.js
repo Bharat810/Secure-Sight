1d9b5688a999a8a03bae64cfbcead20c592b00c9
Bharat810 <81350464+Bharat810@users.noreply.github.com>
2024-05-06 17:29:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const VirusTotal = require('api')('@virustotal/v3.0'); // Assuming you have the SDK installed
 
+const fetch = require('node-fetch');
+const bodyParser = require('body-parser');
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
@@ -26,5 +28,25 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
 //     res.status(500).json({ message: 'Error uploading or scanning file!' }); // Informative error message
 //   }
 // });
+
+//IPAddress
+app.use(bodyParser.json());
+app.post('/check-ip', async (req, res) => {
+  const { ipAddress } = req.body;
+  try {
+      const response = await fetch(https://www.virustotal.com/api/v3/ip_addresses/${ipAddress}, {
+          headers: {
+              'x-apikey': 'YOUR_VIRUSTOTAL_API_KEY',
+          },
+      });
+      const data = await response.json();
+
+      res.json({ isMalicious: data.data.attributes.reputation !== 'none' });
+  } catch (error) {
+      console.error('Error checking IP:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 app.listen(5000, () => console.log('Server listening on port 5000'));
